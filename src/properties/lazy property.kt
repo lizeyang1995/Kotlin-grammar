2012131617c934cb214f7240c47c99a7ae7dc420
lizeyang1995 <424011527@qq.com>
2020-12-28 23:58:49 +0800

    [lizeyang] feat: complete lazy property

--- src/properties/lazy property.kt
+++ src/properties/lazy property.kt
@@ -1,6 +1,18 @@
 package properties
 
 fun main() {
-
+    val init: () -> Int = {1}
+    val lazy = LazyProperty(init).lazy
+    println(lazy)
 }
 
+class LazyProperty(val initializer: () -> Int) {
+    var value: Int? = null
+    val lazy: Int
+        get() {
+            if (value == null) {
+                value = initializer()
+            }
+            return value!!
+        }
+}
